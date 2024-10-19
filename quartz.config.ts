a1f4495320b12a57a71cc8f8a54a619765b78dc8
Jamie Land <hokie10@gmail.com>
2024-10-19 17:28:34 -0400

    Updating image and title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🎵 Composer AI",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
